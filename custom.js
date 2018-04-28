e1630554af0d4eaee94c9d02fcec0c5f38b0a6b5
cc003 <tinestudios@gmail.com>
2018-04-28 22:31:22 +1000

    scrolling nav bar

--- custom.js
+++ custom.js
@@ -88,3 +88,15 @@ $('#carouselExample').on('slide.bs.carousel', function (e) {
 
 });
 
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar-scroll").style.top = "0";
+  } else {
+    document.getElementById("navbar-scroll").style.top = "-178px";
+  }
+  prevScrollpos = currentScrollPos;
+}
